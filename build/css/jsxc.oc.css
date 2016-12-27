239641a83f9446c0b6acef46c0ba064f76377c7a
sualko <klaus@jsxc.org>
2016-12-27 11:44:45 +0100

    build v3.0.2+appstore

--- build/css/jsxc.oc.css
+++ build/css/jsxc.oc.css
@@ -1,5 +1,5 @@
 /*!
- * ojsxc v3.0.2 - 2016-12-23
+ * ojsxc v3.0.2+appstore - 2016-12-27
  * 
  * Copyright (c) 2016 Klaus Herberth <klaus@jsxc.org> <br>
  * Released under the MIT license
@@ -7,7 +7,7 @@
  * Please see http://www.jsxc.org/
  * 
  * @author Klaus Herberth <klaus@jsxc.org>
- * @version 3.0.2
+ * @version 3.0.2+appstore
  * @license MIT
  */
 
