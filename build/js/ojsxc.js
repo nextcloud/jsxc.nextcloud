554ca9c438d2b3a2684b0ffcf0be8620e110cfd9
sualko <klaus@jsxc.org>
2017-05-17 13:36:18 +0200

    build v3.2.0 for nc 12

--- build/js/ojsxc.js
+++ build/js/ojsxc.js
@@ -1,5 +1,5 @@
 /*!
- * ojsxc v3.2.0-beta.3 - 2017-05-02
+ * ojsxc v3.2.0 - 2017-05-17
  * 
  * Copyright (c) 2017 Klaus Herberth <klaus@jsxc.org> <br>
  * Released under the MIT license
@@ -7,7 +7,7 @@
  * Please see http://www.jsxc.org/
  * 
  * @author Klaus Herberth <klaus@jsxc.org>
- * @version 3.2.0-beta.3
+ * @version 3.2.0
  * @license MIT
  */
 
