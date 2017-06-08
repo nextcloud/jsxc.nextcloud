75a9b72fc50b0122e196d526403d7f531d11ef1c
Ján Stibila <stibila@gmail.com>
2017-06-08 12:35:10 +0200

    Fix - preffer mail address to login

--- ajax/getSettings.php
+++ ajax/getSettings.php
@@ -68,7 +68,9 @@ if (validateBoolean($config->getAppValue('ojsxc', 'xmppPreferMail'))) {
 }
 
 if (validateBoolean($config->getAppValue('ojsxc', 'timeLimitedToken'))) {
-   $data['xmpp']['username'] = $currentUID;
+   if($data['xmpp']['username'] == null) {
+       $data['xmpp']['username'] = $currentUID;
+   }
    $jid = $data['xmpp']['username'] . '@' . $data['xmpp']['domain'];
    $expiry = time() + 60*60;
    $secret = $config->getAppValue('ojsxc', 'apiSecret');
