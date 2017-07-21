2bbae434a039d3beefd5274f4e659368fe97f5dc
sualko <klaus@jsxc.org>
2017-07-21 17:02:10 +0200

    remove old routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -10,21 +10,6 @@
 
 use \OCA\OJSXC\AppInfo\Application;
 
-$this->create('ojsxc_ajax_getturncredentials', 'ajax/getTurnCredentials.php')
-	->actionInclude('ojsxc/ajax/getTurnCredentials.php');
-
-$this->create('ojsxc_ajax_setadminsettings', 'ajax/setAdminSettings.php')
-	->actionInclude('ojsxc/ajax/setAdminSettings.php');
-
-$this->create('ojsxc_ajax_setUserSettings', 'ajax/setUserSettings.php')
-	->actionInclude('ojsxc/ajax/setUserSettings.php');
-
-$this->create('ojsxc_ajax_getUsers', 'ajax/getUsers.php')
-	->actionInclude('ojsxc/ajax/getUsers.php');
-
-$this->create('ojsxc_ajax_externalApi', 'ajax/externalApi.php')
-	->actionInclude('ojsxc/ajax/externalApi.php');
-
 $application = new Application();
 $application->registerRoutes($this, array(
 	'routes' => array(
