809232ae7d7c65ba460ca00965fe805fa1664b53
sualko <klaus@jsxc.org>
2020-03-28 18:59:17 +0100

    test: print nextcloud path

--- tests/bootstrap-unit.php
+++ tests/bootstrap-unit.php
@@ -3,5 +3,7 @@ $nc_root = __DIR__ . '/../../..';
 
 @include 'bootstrap-config.development.php';
 
+echo "Using ".realpath($nc_root)." as Nextcloud root.\n\n";
+
 require_once $nc_root . '/lib/base.php';
 require_once __DIR__ . '/../vendor/autoload.php';
